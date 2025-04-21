da219e463f10be7b03ac971fb66b037408f96a11
Vince <vincentsilva900@gmail.com>
2025-04-21 11:38:44 -0700

    eertyhgfdcyh

--- routes/mossbook.js
+++ routes/mossbook.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 const multer = require('multer');
 const { storage } = require('../utils/cloudinary');
 const upload = multer({ storage });
