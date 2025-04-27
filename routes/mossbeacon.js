d7b7c8c833283e9ff89f738686f4ff2b041942d8
Vince <vincentsilva900@gmail.com>
2025-04-27 16:13:55 -0700

    zasxzdeyh

--- routes/mossbeacon.js
+++ routes/mossbeacon.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const MossbeaconMessage = require('../models/MossbeaconMessage'); // <<< ADD THIS LINE
+const MossbeaconMessage = require('../models/MossbeaconMessage'); // Import the Message model
 
 // GET Mossbeacon Chat
 router.get('/', async (req, res) => {
@@ -19,12 +19,15 @@ router.get('/', async (req, res) => {
       return res.redirect('/user/' + user.username);
     }
 
-    // Find other beacon users except yourself
     const otherBeaconUsers = await User.find({ mossbeacon: true, _id: { $ne: userId } });
 
-    res.render('mossbeaconChat', { user, otherBeaconUsers });
+    // NEW: Separate messages
+    const messagesSent = await MossbeaconMessage.find({ sender: userId }).populate('sender receiver');
+    const messagesReceived = await MossbeaconMessage.find({ receiver: userId }).populate('sender receiver');
+
+    res.render('mossbeaconChat', { user, otherBeaconUsers, messagesSent, messagesReceived });
   } catch (err) {
-    console.error(err);
+    console.error('Mossbeacon Error:', err);
     res.status(500).send('Internal Server Error');
   }
 });
@@ -44,9 +47,10 @@ router.post('/send', async (req, res) => {
     await newMessage.save();
     res.redirect('/mossbeacon');
   } catch (err) {
-    console.error(err);
+    console.error('Send Music Link Error:', err);
     res.redirect('back');
   }
 });
 
 module.exports = router;
+
